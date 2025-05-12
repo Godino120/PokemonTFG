d6e68f377a5d3604740bd9d109b7e7132ad0cbac
Godino120 <jgodinorodas@gmail.com>
2025-05-12 18:01:47 +0200

    Algunos cambios que hay que modificar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,13 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { SesionComponent } from '../sesion/sesion.component';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [CommonModule, SesionComponent],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+
+  isSesionModalVisible: boolean = false;
+
+  toggleSesionModal(): void {
+    this.isSesionModalVisible = !this.isSesionModalVisible;
+  }
 
   constructor(
     private router: Router
